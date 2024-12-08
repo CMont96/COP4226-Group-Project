44c5537bf3d2ac75a830cfaeade99cb404328525
Cristian G Montanez <cmont221@fiu.edu>
2024-12-08 07:47:42 -0500

    Add files via upload
    New class/form added which brings up items from the database off partial ID match from the search option

---
+++ BookSearchSelection.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace GroupProject7
+{
+    partial class frmBookSearchSelection
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAddItem = new System.Windows.Forms.Button();
+            this.dgvSearchItems = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSearchItems)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAddItem
+            // 
+            this.btnAddItem.Location = new System.Drawing.Point(242, 262);
+            this.btnAddItem.Name = "btnAddItem";
+            this.btnAddItem.Size = new System.Drawing.Size(110, 40);
+            this.btnAddItem.TabIndex = 0;
+            this.btnAddItem.Text = "Add To Cart";
+            this.btnAddItem.UseVisualStyleBackColor = true;
+            this.btnAddItem.Click += new System.EventHandler(this.btnAddItem_Click);
+            // 
+            // dgvSearchItems
+            // 
+            this.dgvSearchItems.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvSearchItems.Location = new System.Drawing.Point(12, 60);
+            this.dgvSearchItems.Name = "dgvSearchItems";
+            this.dgvSearchItems.Size = new System.Drawing.Size(612, 196);
+            this.dgvSearchItems.TabIndex = 1;
+            // 
+            // frmBookSearchSelection
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(636, 343);
+            this.Controls.Add(this.dgvSearchItems);
+            this.Controls.Add(this.btnAddItem);
+            this.Name = "frmBookSearchSelection";
+            this.Text = "BookSearchSelection";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSearchItems)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAddItem;
+        private System.Windows.Forms.DataGridView dgvSearchItems;
+    }
+}
---
+++ BookSearchSelection.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GroupProject7
+{
+    public partial class frmBookSearchSelection : Form
+    {
+        private frmShoppingCart shoppingCartItem; 
+        public frmBookSearchSelection(frmShoppingCart shoppingCartItem)
+        {
+            InitializeComponent();
+            this.shoppingCartItem = shoppingCartItem;
+        }
+
+        public frmBookSearchSelection() 
+        {
+            InitializeComponent();
+        }
+
+        public void setDataSource(DataTable datatable) 
+        {
+            dgvSearchItems.DataSource = datatable;
+        }
+
+        private void btnAddItem_Click(object sender, EventArgs e)
+        {
+            shoppingCartItem = new frmShoppingCart();
+            if (dgvSearchItems.SelectedRows.Count > 0)
+            {
+                DataGridViewRow selectedRow = dgvSearchItems.SelectedRows[0];
+                // Get the DataRow from the selected row
+                DataRowView dataRowView = (DataRowView)selectedRow.DataBoundItem;
+                // Assuming shoppingCartItem is an instance of frmShoppingCart
+                shoppingCartItem.addItemtoShoppingCart(((DataRowView)selectedRow.DataBoundItem).Row);
+
+                MessageBox.Show("Item added to cart.");
+            }
+        }
+    }
+}
